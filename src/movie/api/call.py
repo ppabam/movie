d49f228ee36b69c9ac0e3a2cea3308d5c8b6e9be
ppabam <tom@diginori.com>
2025-03-17 16:50:53 +0900

    a

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -21,22 +21,23 @@ def call_api(dt="20120101", url_param={}):
     j = data.json()
     return j['boxOfficeResult']['dailyBoxOfficeList']
 
-def list2df(data: list, dt: str):
+def list2df(data: list, dt: str, url_params={}):
     df = pd.DataFrame(data)
     df['dt'] = dt
+    for k, v in url_params.items():
+        df[k] = v
     
     num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',
                 'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten',
                 'salesChange', 'audiInten', 'audiChange']
-
-    #for col_name in num_cols:
-    #    df[col_name] = pd.to_numeric(df[col_name])
     df[num_cols] = df[num_cols].apply(pd.to_numeric) 
 
     return df
 
-def save_df(df, base_path):
-    df.to_parquet(base_path, partition_cols=['dt'])
-    save_path = f"{base_path}/dt={df['dt'][0]}"
+def save_df(df, base_path, partitions=['dt']):
+    df.to_parquet(base_path, partition_cols=partitions)
+    save_path = base_path
+    for p in partitions:
+        save_path = save_path + f"/{p}={df[p][0]}"
     return save_path
 
