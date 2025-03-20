143d5ef26fe879c04fa1fe5f522667cf15edc9c0
ppabam <tom@diginori.com>
2025-03-20 11:36:51 +0900

    2503.20.0

--- src/movie/api/after.py
+++ src/movie/api/after.py
@@ -2,17 +2,18 @@ import pandas as pd
 
 
 def fillna_meta(previous_df: pd.DataFrame, current_df: pd.DataFrame) -> pd.DataFrame:
+    if previous_df is None:
+        return current_df[["movieCd", "multiMovieYn", "repNationCd"]]
+    
     # movieCd 기준으로 outer join
-    merged_df = A.merge(B, on="movieCd", how="outer", suffixes=("_A", "_B"))
+    merged_df = previous_df.merge(current_df, on="movieCd", how="outer", suffixes=("_A", "_B"))
     print(merged_df)
 
     # NaN 값 대체 (A 값 우선, 없으면 B 값 사용)
-    merged_df["multiMovieYn"] = merged_df["multiMovieYn_A"].combine_first(
-        merged_df["multiMovieYn_B"]
-    )
-    merged_df["repNationCd"] = merged_df["repNationCd_A"].combine_first(
-        merged_df["repNationCd_B"]
-    )
+    merged_df["multiMovieYn"] = merged_df["multiMovieYn_A"].combine_first(merged_df["multiMovieYn_B"])
+    merged_df["repNationCd"] = merged_df["repNationCd_A"].combine_first(merged_df["repNationCd_B"])
 
     # 불필요한 컬럼 제거
-    merged_df = merged_df[["movieCd", "multiMovieYn", "repNationCd"]]
+    meta_df = merged_df[["movieCd", "multiMovieYn", "repNationCd"]]
+    
+    return meta_df
--- tests/test_after.py
+++ tests/test_after.py
@@ -3,7 +3,7 @@ from movie.api.after import fillna_meta
 
 
 def test_fillna_meta():
-    A = pd.DataFrame(
+    previous_df = pd.DataFrame(
         {
             "movieCd": ["1001", "1002", "1003"],
             "multiMovieYn": ["Y", "Y", "N"],
@@ -11,7 +11,7 @@ def test_fillna_meta():
         }
     )
 
-    B = pd.DataFrame(
+    current_df = pd.DataFrame(
         {
             "movieCd": ["1001", "1003", "1004"],
             "multiMovieYn": [None, "Y", "Y"],
@@ -19,6 +19,21 @@ def test_fillna_meta():
         }
     )
 
-    r_df = fillna_meta(A, B)
+    r_df = fillna_meta(previous_df, current_df)
 
-    assert True
+    assert not r_df.isnull().values.any(), "결과 데이터프레임에 NaN 또는 None 값이 있습니다!"
+    
+def test_fillna_meta_none_previous_df():
+    previous_df = None
+
+    current_df = pd.DataFrame(
+        {
+            "movieCd": ["1001", "1003", "1004"],
+            "multiMovieYn": [None, "Y", "Y"],
+            "repNationCd": [None, "F", "K"],
+        }
+    )
+
+    r_df = fillna_meta(previous_df, current_df)
+
+    assert r_df.equals(current_df), "r_df는 current_df와 동일해야 합니다!"
