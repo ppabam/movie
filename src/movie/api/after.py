2cdb17076d716fd06b88cae151b16d5d750ce902
ppabam <tom@diginori.com>
2025-03-21 14:05:47 +0900

    2503.21.1

--- src/movie/api/after.py
+++ src/movie/api/after.py
@@ -27,7 +27,7 @@ def fillna_meta(previous_df: pd.DataFrame, current_df: pd.DataFrame) -> pd.DataF
 
 
 def combine_df(meta_df: pd.DataFrame, current_df: pd.DataFrame, ds_nodash: str) -> pd.DataFrame:
-    merged_df = meta_df.merge(current_df, on="movieCd", how="left", suffixes=("_meta", "_current"))
+    merged_df = current_df.merge(meta_df, on="movieCd", how="left", suffixes=("_current", "_meta"))
     merged_df["multiMovieYn"] = merged_df["multiMovieYn_meta"].combine_first(merged_df["multiMovieYn_current"])
     merged_df["repNationCd"] = merged_df["repNationCd_meta"].combine_first(merged_df["repNationCd_current"])
     final_df = merged_df[current_df.columns]
